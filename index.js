36c2f74105d864853ed1157c34eb75c2d9223ad7
wajd alkayyali <wajd.kayyali@gmail.com>
2023-09-27 11:33:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+{let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);} 
+
+
+{
+    let random = Math.floor(Math.random() * 10) + 1;
+    console.log(random)
+}
+
+
+{ let num3 = 10
+let num4 =14
+let mod = num4 % num3
+console.log(mod)}
+
+{ 
+let x = 10
+let y = 12
+let w = 20
+
+let max = Math.max(x,y,w);
+console.log(max)
+}
